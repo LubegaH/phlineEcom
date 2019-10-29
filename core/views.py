4e4e1a6dc2189d3c88c1262304686af6be12b06b
Ham <lubegah@gmail.com>
2019-10-29 23:36:28 +0300

    Added Shopping cart logic

--- core/views.py
+++ core/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item, OrderItem, Order
+from django.views.generic import ListView, DetailView
+from django.utils import timezone
 
-# Create your views here.
+
+def checkout(request):
+    return render(request, 'checkout-page.html')
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
+
+
+# Add to Shopping cart logic
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user, ordered=False)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+        # Check if order_item is in the order
+        if order.item.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+
+        else:
+            order.item.add(order_item)
+
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+
+    return redirect("core:product", slug=slug)
+
+
+# Remove from shopping cart logic
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+        # Check if order_item is in the order
+        if order.item.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.item.remove(order_item)
+        else:
+            # Message stating order does not contain this order item
+            return redirect("core:product", slug=slug)
+
+
+    else:
+        # Message indicating user has no order
+        return redirect("core:product", slug=slug)
+
+    return redirect("core:product", slug=slug)
--- phlineEcom/settings.py
+++ phlineEcom/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 
     # For Authentication
     'django.contrib.sites',
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'phlineEcom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,9 +122,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 
 # Authentication
