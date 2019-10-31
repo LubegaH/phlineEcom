bf4086363e819a3af15c771df72730312f446666
Ham <lubegah@gmail.com>
2019-10-31 15:40:24 +0300

    Adding to cart and removing from cart functionality added

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import Item, OrderItem, Order
 from django.views.generic import ListView, DetailView
 from django.utils import timezone
@@ -32,15 +33,18 @@ def add_to_cart(request, slug):
         if order.item.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
+            messages.info(request, "Item quantity has been updated")
 
         else:
             order.item.add(order_item)
+            messages.info(request, "This item has been added to your cart")
+            return redirect("core:product", slug=slug)
 
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
-
+        messages.info(request, "This item has been added to your cart")
     return redirect("core:product", slug=slug)
 
 
@@ -49,7 +53,8 @@ def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(
         user=request.user,
-        ordered=False)
+        ordered=False
+    )
 
     if order_qs.exists():
         order = order_qs[0]
@@ -61,13 +66,15 @@ def remove_from_cart(request, slug):
                 ordered=False
             )[0]
             order.item.remove(order_item)
+            messages.info(request, "This item has been removed from your cart")
+            return redirect("core:product", slug=slug)
         else:
             # Message stating order does not contain this order item
+            messages.info(request, "This item was not in your cart")
             return redirect("core:product", slug=slug)
 
 
     else:
         # Message indicating user has no order
+        messages.info(request, "You do not have an active order")
         return redirect("core:product", slug=slug)
-
-    return redirect("core:product", slug=slug)
